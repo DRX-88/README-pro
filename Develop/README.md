371200b72293f33d34e5d846cc891f32a92f5d86
DRX-88 <nrmj02@gmail.com>
2024-05-22 01:13:11 -0400

    added to index.js added questions and choices for license for cmd line

---
+++ Develop/README.md
@@ -0,0 +1,40 @@
+# My awesome project
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [Questions](#questions)
+- [License](#license)
+
+## Description
+The coolest application ever
+
+## Installation
+Please run the following command to install the required dependencies:
+$npm i
+
+## Usage
+Please run the following command to run the application:
+$node index.js
+
+## Tests
+Please run the following command to run the tests:
+$npm run tests
+
+## Contributing
+Feel free to contribute to this project by following the guidelines below:
+undefined
+
+## Questions
+If you have any questions, you can reach me through the following:
+- GitHub: [undefined](
+- Email: undefined
+
+## License
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const { type } = require('os');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'welcome',
+        message: 'Please provide a welcome message for your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information for your project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide contribution guidelines for your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project.',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'no license']
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            writeToFile('README.md', generateMarkdown(data));
+        });
+}
 
 // Function call to initialize app
 init();
+
